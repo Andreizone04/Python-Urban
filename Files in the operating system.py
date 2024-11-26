51b9cb6c55fa7173f0cba24297d52a982b0454db
Andreizone04 <130288785+Andreizone04@users.noreply.github.com>
2024-11-27 00:07:17 +0300

    Add files via upload

---
+++ Files in the operating system.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+for root, dirs, files in os.walk(r'C:\Users\Andre\PycharmProjects\Python7'):
+    for file in files:
+
+        filepath = os.path.abspath(file)
+
+        filetime = os.stat(file).st_mtime
+
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        filesize = os.stat(file).st_size
+
+        parent_dir = os.path.dirname(filepath)
+
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
