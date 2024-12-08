85ea7a5e590e458bfe5121a116eaf79dc1adae53
Andreizone04 <130288785+Andreizone04@users.noreply.github.com>
2024-12-08 21:39:47 +0300

    Add files via upload

--- task_9_3.py
+++ task_9_3.py
@@ -3,7 +3,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (abs(len(i) - len(j)) for i, j in zip(first, second) if len(i) != len(j))
 
-second_result = (bool(len(first[i]) - len(second[i])==0)  for i in range(len(first)))
+second_result = (bool(len(first[i]) - len(second[i]) == 0) for i in range(len(first)))
 
 print(list(first_result))
 
