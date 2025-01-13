9e6565b1f5c4faf738cd4f61b120c1a08b654c51
Andreizone04 <130288785+Andreizone04@users.noreply.github.com>
2025-01-13 19:28:47 +0300

    Add files via upload

--- task_11_3.py
+++ task_11_3.py
@@ -1,7 +1,13 @@
 import pprint
 
 def introspection_info(obj):
-    type_info = {'Тип':type(obj),'атрибуты и методы объекта':dir(obj)}
+    type_info = {'Тип': type(obj),
+        'Атрибуты и Методы': obj.__dict__.keys() if hasattr(obj, '__dict__') else dir(obj),
+        'Имя': obj.__name__ if hasattr(obj, '__name__') else 'Нет имени',
+        'Модуль': obj.__module__ if hasattr(obj, '__module__') else 'Не модуль',
+        'Документация': obj.__doc__,
+        'Можно ли вызвать': callable(obj),
+        'Идентификатор': id(obj),}
     return type_info
 
 
